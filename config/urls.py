0a944672bf7e93c22c27e29ce576c11b6350b932
Thomas Morris <tomchrismorris@yahoo.com>
2022-08-25 20:59:42 -0400

    tried to setup index.html but getting a 404

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from flashcards import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.default.urls')),
 ]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='flashcards', permanent=False)),
+]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+def index(request):
+
+    context = {}
+
+    return render(request, 'index.html', context=context)

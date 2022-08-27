3d908d9539a19d74c9f83bdf55b5f1bcdc8e03eb
Thomas Morris <tomchrismorris@yahoo.com>
2022-08-26 21:50:34 -0400

    added code for models and registered them in admin, need to code urls and views

--- flashcards/admin.py
+++ flashcards/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Answer
+from .models import User, Answer, Question, Card, Box, Subject
 
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
 admin.site.register(Answer)
+admin.site.register(Question)
+admin.site.register(Card)
+admin.site.register(Box)
+admin.site.register(Subject)
--- flashcards/models.py
+++ flashcards/models.py
@@ -20,3 +20,59 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.answer_content
+
+
+class Question(models.Model):
+    question_content = models.TextField(
+        max_length=255,
+        help_text='Record question here'
+        )
+
+    def get_absolute_url(self):
+        return reverse('question-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.question_content
+
+
+class Card(models.Model):
+    question = models.ForeignKey(
+        'Question',
+        on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='questions'
+        )
+    answer = models.ForeignKey(
+        'Answer',
+        on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='answers'
+        )
+
+    def get_absolute_url(self):
+        return reverse('card-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'The question is {self.question} and the answer is {self.answer}'
+
+
+class Box(models.Model):
+    name = models.IntegerField(blank=True, null=True)
+    cards = models.ManyToManyField('Card', related_name="boxes")
+
+    def get_absolute_url(self):
+        return reverse('box-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter Subject')
+    boxes = models.ManyToManyField('Box', related_name="subjects")
+
+    def get_absolute_url(self):
+        return reverse('subject-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name

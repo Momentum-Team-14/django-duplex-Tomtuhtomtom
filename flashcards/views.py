5df7b7c38503dd27227451fd4bf67b295edd33f2
Thomas Morris <tomchrismorris@yahoo.com>
2022-09-01 16:14:14 -0400

    messing with data structure, wanted to commit minor changes while it's working before trying anything else

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -7,8 +7,8 @@ from django.views.generic import (
     DeleteView,
 )
 
-from .models import Card
-from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Card, User
 from .forms import CardCheckForm
 
 import random
